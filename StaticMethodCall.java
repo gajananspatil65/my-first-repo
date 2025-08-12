47e62135a99e703cc481b6bdcc2f707ae73dbfb3
gajanan <gajananspatil65@gmail.com>
2025-08-12 17:10:57 +0530

    methods

---
+++ StaticMethodCall.java
@@ -0,0 +1,24 @@
+package method;
+
+public class StaticMethodCall {
+	//static method call in same class
+	public static void m1()
+	{
+		System.out.println("m1 get called");
+	}
+public static void m2()
+{
+	System.out.println("m2 get called");
+}
+public static void m3()
+{
+	System.out.println("m3 get called");
+}
+	public static void main(String[] args) {
+	m1();
+	m2();
+	StaticMethodCall.m3();//but no need to write class name to call in same class
+	
+	}
+
+}
---
+++ StaticMethodCallInDiffClass.java
@@ -0,0 +1,13 @@
+package method;
+
+public class StaticMethodCallInDiffClass {
+	//static method call from different class
+
+	public static void main(String[] args) {
+		StaticMethodCall.m1();
+		StaticMethodCall.m2();
+		StaticMethodCall.m3();
+
+	}
+
+}
